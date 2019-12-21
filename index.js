ee58b38716465b02be38c5c557ecd35fcb73c483
Leandro Haruki Futema <leandroharuki@gmail.com>
2019-12-20 22:18:41 -0300

    Adding an infinite email loop

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const nodemailer = require('nodemailer')
+const cron = require('node-cron')
 const User = require('./Models/User')
 
 const app = express()
@@ -12,7 +14,7 @@ mongoose
 app.get('/', async (req, res) => {
   try {
     const newUser = new User({
-      email: 'teste@teste.com',
+      email: 'leandroharuki@gmail.com',
       interval: 69420911,
     })
     const persistedUser = await newUser.save()
@@ -20,6 +22,34 @@ app.get('/', async (req, res) => {
   } catch (error) {
     res.status(500).send({ message: 'Deu ruim!', error })
   }
+})
+
+app.get('/mail', (req, res) => {
+  const mailOptions = {
+    from: 'teste@teste.com',
+    to: 'fulano@teste.com',
+    subject: 'Email from Node-App: A Test Message!',
+    text: 'Some content to send',
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: '7961a02b9d9d04',
+      pass: 'c0e17454989f1e',
+    },
+  })
+  const task = cron.schedule('* * * * *', () => {
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        res.status(500).send(error)
+      } else {
+        res.send({ message: 'Email sent', info: info.response })
+      }
+    })
+  })
+  task.start()
 })
 
 app.listen(3000, () => {
