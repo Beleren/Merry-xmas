8e8a1e5a8ee03f2596300d0f92094bd7f388d55f
Leandro Haruki Futema <leandroharuki@gmail.com>
2019-12-22 20:12:49 -0300

    Adding node-sass and basic form validations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
 // Render Prop
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import './App.scss'
 
 const App = () => (
   <div>
     <h1>Any place in your app!</h1>
     <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        const errors = {}
-        if (!values.email) {
-          errors.email = 'Required'
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address'
-        }
-        return errors
-      }}
+      initialValues={{ email: '', search: '', interval: '' }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .required('Please fill the email field')
+          .email('This is not a valid email address!'),
+        search: Yup.string()
+          .max(50, 'Search text is too long!')
+          .required('Please fill the search field'),
+        interval: Yup.number()
+          .min(120)
+          .max(1800)
+          .required('Please choose an interval to send email'),
+      })}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2))
@@ -25,12 +28,33 @@ const App = () => (
         }, 400)
       }}
     >
-      {({ isSubmitting }) => (
+      {({ isSubmitting, handleChange, handleBlur, values }) => (
         <Form>
           <Field type="email" name="email" />
           <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
+          <Field type="text" name="search" />
+          <ErrorMessage name="search" component="div" />
+          <select
+            name="interval"
+            value={values.interval}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            style={{ display: 'block' }}
+          >
+            <option value="" label="Select an interval">
+              Select an interval
+            </option>
+            <option value="120" label="2 minutes">
+              2 minutes
+            </option>
+            <option value="600" label="10 minutes">
+              10 minutes
+            </option>
+            <option value="1800" label="30 minutes">
+              30 minutes
+            </option>
+          </select>
+          <ErrorMessage name="interval" component="div" />
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
